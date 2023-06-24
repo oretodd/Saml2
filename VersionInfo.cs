fdad0a2e4a3b2dc9076effd8e69f548578af053c
Todd Robertson <trobertson@oteemo.com>
2023-06-24 12:53:29 -0700

    Enable configuration of specific SP endpoint paths, such as the ACS, signin, & logout

---
+++ VersionInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("Sustainsys")]
+[assembly: AssemblyCopyright("Copyright © Sustainsys and contributors 2013-2023")]
+
+
+// Sustainsys.Saml2 uses semantic versioning
+//
+//      Major Version
+//      Minor Version 
+//      Patch Number
+//
+
+[assembly: AssemblyVersion("2.9.1")]
+[assembly: AssemblyFileVersion("2.9.1")]
+[assembly: AssemblyInformationalVersion("2.9.1")]
